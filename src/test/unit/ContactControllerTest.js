b7b731ae0191c40c348d6e832622bf05d03e5fb0
Sergey <sergey.kosik@gmail.com>
2013-10-07 09:07:48 +0100

    Controller test is working now - Thanks to Marco.

--- src/test/unit/ContactControllerTest.js
+++ src/test/unit/ContactControllerTest.js
@@ -1,22 +1,32 @@
-define(function() {
+/**
+ * the controller needs to be loaded explicitly with requireJS as the normal application only registers the
+ * controllers inside the route definitions, which are not evaluated during testing, so they are not known to angularJS
+ */
+define(["contact/ContactController"], function(ContactController) {
+
 	"use strict";
 	describe("the contactcontroller", function () {
 		var contactController, scope;
 
 		beforeEach(function () {
-			//load contact module,    see http://docs.angularjs.org/api/angular.mock.inject
+			//load contact module, see http://docs.angularjs.org/api/angular.mock.inject
 			module("contact");
 
-			//properly instantiate the service
-			inject(["ContactController", function (_contactController) {
-				contactController = _contactController;
+			inject(["$rootScope", "$controller", function ($rootScope, $controller) {
+				//instantiate the controller with a newly created scope
+				scope = $rootScope.$new();
+				contactController = $controller(ContactController, {$scope: scope});
+				//this would fail -> $controller("ContactController", {$scope: scope});
 			}]);
 		});
 
 
-		it("should attach a list of contacts to the scope", function () {
-			//expect(scope.contacts.length).toBe(2);
-			expect(true).toBe(true);
+		it("should be the authors name", function () {
+			expect(scope.author).toBe("Marco Rinck");
+		});
+
+		it("should attach contacts to the scope", function () {
+			expect(scope.contacts.length).toBe(2);
 		});
 	});
 
